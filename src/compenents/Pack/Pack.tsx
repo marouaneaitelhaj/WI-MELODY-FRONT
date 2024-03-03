bd7c4045d77f1f6d4b77fd56641b3093ed82163c
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-03-03 22:11:54 +0000

    MEL-1 MEL-2 MEL-3 MEL-4 MEL-5 MEL-6 MEL-7 MEL-8 MEL-9 MEL-10 MEL-11 MEL-12 MEL-13 MEL-14 MEL-15 MEL-16 MEL-17 MEL-18 MEL-19 MEL-20 MEL-21 MEL-22 MEL-23 MEL-24 MEL-25 MEL-26 MEL-27 MEL-28 MEL-29 MEL-30 MEL-31 MEL-32 MEL-33 MEL-34 MEL-35 MEL-36 MEL-37 MEL-38 MEL-39 MEL-40 MEL-41 MEL-42 MEL-43 MEL-44 MEL-45 MEL-46 MEL-47 MEL-48 MEL-49 MEL-50 MEL-51 MEL-52 MEL-53 MEL-54 MEL-55 MEL-56 MEL-57 MEL-58 MEL-59 MEL-60 MEL-61 MEL-62 MEL-63 MEL-64 MEL-65

--- src/compenents/Pack/Pack.tsx
+++ src/compenents/Pack/Pack.tsx
@@ -16,36 +16,7 @@ export function Pack(props: { pack: Tpack }) {
                     className="w-full h-auto rounded-t-lg aspect-w-1 aspect-h-1 overflow-hidden hover:brightness-75 cursor-pointer duration-500 ease-in-out"
                     width="300"
                     height="300"
-                // style="aspect-ratio: 300 / 300; object-fit: cover;"
                 />
-                <div className="absolute top-0 left-0 p-4">
-                    {/* <div className="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80">
-                        {props.pack.name}
-                    </div> */}
-                </div>
-                <div className="absolute bottom-0 left-0 p-4">
-                    {/* <div className="flex items-center space-x-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="text-white"
-                        >
-                            <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
-                            <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                        </svg>
-                        <span className="text-white">Locked</span>
-                    </div> */}
-                </div>
-                {/* <div className="absolute bottom-0 right-0 p-4 bg-[#00000080] text-white rounded"> */}
-                    {/* <span className="text-xs font-semibold">{props.pack.tier.name}</span> */}
-                {/* </div> */}
             </div>
             <div className="p-4 bg-white rounded-b-lg">
                 <h3 className="text-lg font-bold">{props.pack.name}</h3>
