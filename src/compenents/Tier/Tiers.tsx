3d70c2f0bb7c368d16a7ef7011781533835db237
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-03-11 15:04:25 +0000

    Update tier rendering in Tiers component

--- src/compenents/Tier/Tiers.tsx
+++ src/compenents/Tier/Tiers.tsx
@@ -4,10 +4,10 @@ import Tier from "./TierCard";
 export default function Tiers(props: { selectedArtist: Tuser | null }) {
     return (
         <div className="my-5  w-full bg-gray-100 flex flex-col items-center h-96">
-            <h1 className="text-3xl my-3">Choose your tier</h1>
+            <div className="text-3xl my-3">Choose your tier</div>
             <div className="flex">
                 {props.selectedArtist?.tiers.map((tier) => (
-                    <Tier tier={tier} />
+                    <Tier tier={tier} key={tier.id} />
                 ))}
             </div>
         </div>
